ab2fb11b172ef3f8e2d336dd10b925723dc13eee
0xNaxzerrr <naxdev75@gmail.com>
2024-10-10 12:29:08 +0200

    :sparkles: tokens contract ERC20

---
+++ orderbook/src/MyTokenB.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyTokenB is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TokenB", "TB") {
+        // Créer un nombre initial de tokens pour l'adresse qui déploie le contrat
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+
+    // Optionnel: Cette fonction retourne le nombre de décimales utilisées
+    function decimals() public view virtual override returns (uint8) {
+        return 18; // 18 décimales, comme c'est courant dans les ERC20
+    }
+}
